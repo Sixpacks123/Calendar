0c43551a4d5947f42ae3a33c9fd78054708378f6
Heurtault Aubin <aub.heurtault@gmail.com>
2024-05-06 14:05:36 +0200

    chore: Add School model for managing school data

---
+++ api/app/Models/school.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class School extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'address',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+
+    // Vous pouvez ajouter d'autres méthodes et relations ici
+}
