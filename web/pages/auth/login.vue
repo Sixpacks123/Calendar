f54a68a4341db60526abbfecf4000c2b47d89a0f
Heurtault Aubin <aub.heurtault@gmail.com>
2024-05-06 14:06:26 +0200

    chore: Update login page heading to include "Calendar"

--- web/pages/auth/login.vue
+++ web/pages/auth/login.vue
@@ -5,7 +5,7 @@ definePageMeta({
 </script>
 <template>
   <UCard class="w-full max-w-md mx-auto my-20">
-    <h1 class="text-3xl font-black mb-6 leading-tight tracking-tight">Log In</h1>
+    <h1 class="text-3xl font-black mb-6 leading-tight tracking-tight text">Log In Calendar</h1>
     <AuthLogin />
   </UCard>
 </template>
