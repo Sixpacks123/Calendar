bd9674d71a095c8cb7153a5622c4a6588e6cb8c9
Heurtault Aubin <aub.heurtault@gmail.com>
2024-05-09 16:12:58 +0200

    feat: Add MeetingFactory for generating meeting instances

---
+++ api/database/factories/MeetingFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\School;
+
+class MeetingFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'start_hour' => $this->faker->time(),
+            'end_hour' => $this->faker->time(),
+            'break_time' => $this->faker->numberBetween(5, 30),
+            'location' => $this->faker->city(),
+            'school_id' => School::factory(),
+            'admin_id' => User::factory(),
+            'trainer_id' => User::factory()
+        ];
+    }
+}
